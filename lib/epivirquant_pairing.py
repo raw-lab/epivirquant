bd1e88bc066058a57502624509e99cb0208f677e
Sadie Hollenack <sadiehollenack@gmail.com>
2025-07-14 11:12:51 -0400

    Fixed the virgillian pairing function

--- lib/epivirquant_pairing.py
+++ lib/epivirquant_pairing.py
@@ -24,7 +24,7 @@ def getVP(outDir, snameVP, XBVP, pad):
     plt.imshow(XBVP, cmap='gray')
     plt.title("XB-" + snameVP)
     plt.savefig(f"{outDir}{fsep}Step-1_VP{fsep}XB-{snameVP}", dpi=150)
-    
+
     print('o-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-o\n')
     print("Step 1 begin:\nScanning calibration image for VP candidates...")
     
@@ -55,6 +55,7 @@ def getVP(outDir, snameVP, XBVP, pad):
     proxVec = []
     
     for i, currCenter in enumerate(cVec):
+        print([currCenter])
         dVec = np.transpose(cdist([currCenter], cVec))
         for j, dist in enumerate(dVec):
             if 0 < dist <= dConstraint:
@@ -72,39 +73,39 @@ def getVP(outDir, snameVP, XBVP, pad):
     
     bBox = np.array(bBox)
     VPidx = np.arange(0, len(dIdx), 2)
-    VPcoords = [np.concatenate((bBox[dIdx[k]], bBox[dIdx[k+1]])) for k in VPidx]
+    VPcoords = [np.concatenate([(bBox[dIdx[k], 0:], bBox[dIdx[k+1], 0:])]) for k in VPidx]
     
     VPs = []
-    #changed len(cVec) to len(cVec)-1
-    VPcent = [np.concatenate((cVec[n], cVec[n+1])) for n in range(0, len(cVec)-1, 2)]
-    for VPtemp in VPcoords:
-        xVPmax = int(np.max(VPtemp[3])) + pad
-        xVPmin = int(np.min(VPtemp[1])) - pad
-        yVPmax = int(np.max(VPtemp[2])) + pad
-        yVPmin = int(np.min(VPtemp[0])) - pad
+    cVec = []
+    for i, VPtemp in enumerate(VPcoords):
+        xVPmax = int(np.max(VPtemp[:, 3])) + pad
+        xVPmin = int(np.min(VPtemp[:, 1])) - pad
+        yVPmax = int(np.max(VPtemp[:, 2])) + pad
+        yVPmin = int(np.min(VPtemp[:, 0])) - pad
         VP = XBVP[yVPmin:yVPmax, xVPmin:xVPmax]
         threshold = threshold_otsu(VP)
         otsuMask = VP > threshold
         otsuMask_eroded = ndimage.binary_erosion(otsuMask, iterations=1)
         labels = measure.label(otsuMask_eroded)
         rprops = measure.regionprops(labels, intensity_image=VP)
-        VPs.append(VP)
-    
+        if len(rprops) == 2:
+            for prop in rprops:
+                cVec.append(prop.centroid)
+                VPs.append(VP)
+        else:
+            del proxVec[i]
+
     dMin = np.argmin(proxVec)
+    VPDict = {array.tobytes(): array for array in VPs}
+    VPs = list(VPDict.values())
     VP0 = VPs[dMin]
-    
-    x1, y1, x2, y2 = VPcent[0]
+    VPcent = [np.concatenate((cVec[n], cVec[n+1])) for n in range(0, len(cVec)-1, 2)]
+    x1, x2, y1, y2 = VPcent[0]
     m = (y2 - y1) / (x2 - x1)
-    bSlope = y1 - (m * x1)
-    theta0 = np.arctan(m / 1) * (180 / np.pi)
-    
-    if -5 < theta0 < 5:
-        optBox = VP0
-    else:
-        optBox = ndimage.rotate(VP0, theta0 if theta0 > 5 else -theta0, mode='constant')
-    
+    optBox = VP0
     plt.figure("VP_Final: rotated")
     plt.imshow(optBox, cmap='gray')
+    plt.plot([x2, y2], [x1, y1],color="blue",marker="o",markerfacecolor="r")
     plt.title("VP_Final: rotated")
     plt.savefig(f"{outDir}{fsep}Step-1_VP{fsep}XB-{snameVP}_VP_Final.png", dpi=150)
     plt.close('all')
@@ -116,11 +117,3 @@ def getVP(outDir, snameVP, XBVP, pad):
     print(f"\nStep 1 end: {round(stop, 4)} seconds")
     print('\no-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-o')
     return optBox
-
-# Example usage:
-out_directory = "EpiVirQuant_Output"
-sample_name = "sample"
-XBVP_data = np.random.rand(100, 100)
-padding = 5
-#getVP(out_directory, sample_name, XBVP_data, padding)
-
