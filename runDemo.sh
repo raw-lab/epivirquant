b85da4dcb1b7a129d27ebf5777e02d56dad860ce
Sadie Hollenack <sadiehollenack@gmail.com>
2025-10-20 13:17:55 -0400

    Tweaks to the runDemo script

--- runDemo.sh
+++ runDemo.sh
@@ -24,4 +24,4 @@ echo "Running EpiVirQuant with calibration image GSL-01"
 epivirquant.py --dapi "data/GSL/tiff/dapi" --fitc "data/GSL/tiff/fitc" --calibration "data/GSL/tiff/dapi/GSL_+_blue_beads_1_(dapi).tiff"
 
 echo "EpiVirQuant finished. To run EpiVirQuant on your own data change the --dapi, --fitc, and --calibration flag \
-to you data. More information can be founf by running epivirquant.py --help"
+to where your data is. More information can be found by running epivirquant.py --help"
