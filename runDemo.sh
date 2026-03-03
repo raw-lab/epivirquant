78d42ed33ab4aa57367185b3a1f6e35ab6ec4b80
Jose Figueroa <jose@decrevi.net>
2026-03-03 09:48:14 -0500

    Updated run command for runDemo

--- runDemo.sh
+++ runDemo.sh
@@ -21,7 +21,7 @@ pip install .
 
 echo "Running EpiVirQuant with calibration image GSL-01"
 
-epivirquant.py --dapi "data/GSL/tiff/dapi" --fitc "data/GSL/tiff/fitc" --calibration "data/GSL/tiff/dapi/GSL_+_blue_beads_1_(dapi).tiff"
+epivirquant --dapi "data/GSL/tiff/dapi" --fitc "data/GSL/tiff/fitc" --calibration "data/GSL/tiff/dapi/GSL_+_blue_beads_1_(dapi).tiff"
 
 echo "EpiVirQuant finished. To run EpiVirQuant on your own data change the --dapi, --fitc, and --calibration flag \
 to where your data is. More information can be found by running epivirquant.py --help"
